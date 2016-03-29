8aaee9d2a75150cf74c28ea676db1ccc763fdc9b
Igor Lessa Robaina de Souza <igorlrds@outlook.com>
2016-03-29 00:05:36 -0300

    ...

---
+++ SisDogHotelDAL/Persistence/ConexaoDAL.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace SisDogHotelDAL.Persistence
+{
+    public class ConexaoDAL
+    {
+        protected SqlConnection Con;
+        protected SqlCommand Cmd;
+        protected SqlDataReader Dr;
+
+        protected void OpenConnection()
+        {
+            Con = new SqlConnection(ConfigurationManager.ConnectionStrings["tcc"].ConnectionString);
+
+            Con.Open();
+        }
+        protected void CloseConnection()
+        {
+            Con.Close();
+        }
+    }
+}
